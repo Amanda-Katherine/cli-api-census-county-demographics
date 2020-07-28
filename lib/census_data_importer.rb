3390b6f2e1fa509729006fd2b359ceb31de2665d
Amanda_Katherine <walker.akw@gmail.com>
2020-07-28 16:19:02 -0400

    creates 2018 class variables in the API importer class

--- lib/census_data_importer.rb
+++ lib/census_data_importer.rb
@@ -44,6 +44,23 @@ class DataImporter
     }
 
 
-    @@ variables_hash_2018 = {}
+    @@ variables_hash_2018 = {
+        "NAME": name,
+        "DP05_0001E": population,
+        "DP05_0018E": median_age,
+        "DP03_0062E": median_household_income,
+        "DP05_0037E": white_pop,
+        "DP05_0070E": hisp_latino_pop,
+        "DP05_0038E": black_pop,
+        "DP05_0044E": asian_pop,
+        "DP05_0035E": two_or_more_races_pop,
+        "DP04_0110E": total_num_household_units,
+        "DP04_0111E": less_than_20_percent_income_per_month,
+        "DP04_0112E": btw_20_and_24_9_percent_income_per_month,
+        "DP04_0113E": btw_25_and_29_9_percent_income_per_month,
+        "DP04_0114E": btw_30_and_34_9_percent_income_per_month,
+        "DP04_0115E": 35_pecent_or_more_percent_income_per_month,
+        "DP03_0074E": num_households_with_SNAP_food_stamp
+    }
     
 end
