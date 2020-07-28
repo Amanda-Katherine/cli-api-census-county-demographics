c1c709c275c835f9ded0d448d3fa2b17557bd8e0
Amanda_Katherine <walker.akw@gmail.com>
2020-07-28 01:39:58 -0400

    sets up gems and file interaction in environment.rb

---
+++ bin/run.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+require_relative "../lib/command_line_interface.rb"
+require_relative "../config/environment.rb"
+
+
+CommandLineInterface.new.run
---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+require 'pry'
+require 'open-uri'
+require 'nokogiri'
+require 'net/http'
+require 'json'
+
+require_relative "../lib/census_data_importer.rb"
+require_relative "../lib/cli.rb"
+require_relative "../lib/county_data.rb"
