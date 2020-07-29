8f486a4041ffe52bc6919f515eb34eaa00b241ff
Amanda_Katherine <walker.akw@gmail.com>
2020-07-29 09:08:06 -0400

    fixes errors in the DataImporter class when the api data is being parsed.

--- lib/census_data_importer.rb
+++ lib/census_data_importer.rb
@@ -12,27 +12,26 @@ class DataImporter
 
        #full url: "https://api.census.gov/data/2018/acs/acs5/profile?get=NAME,DP05_0001E,DP05_0018E,DP03_0062E,DP05_0037E,DP05_0070E,DP05_0038E,DP05_0044E,DP05_0035E,DP04_0110E,DP04_0111E,DP04_0112E,DP04_0113E,DP04_0114E,DP04_0115E,DP03_0074E&for=county:*&in=state:*&key=0c47ebad1cc342bbf5eeb2ef26ede29c97ccb260"
 
-    def self.import_list_of_counties_for_cli
-        url = "https://api.census.gov/data/2018/acs/acs5/profile?get=NAME&for=county:*&in=state:*"
+    # def self.import_list_of_counties_for_cli
+    #     url = "https://api.census.gov/data/2018/acs/acs5/profile?get=NAME&for=county:*&in=state:*"
 
-        all_counties = HTTParty.get(url).parsed_response
-        response = response.delete_at(0)
-        response
-    end
+    #     all_counties = HTTParty.get(url).parsed_response
+    #     response = all_counties.delete_at(0)
+    #     response
+    # end
 
-    def self.import_county_data_from_api(county = *, state = *)
-        url = "https://api.census.gov/data/2018/acs/acs5/profile?get=NAME,DP05_0001E,DP05_0018E,DP03_0062E,DP05_0037E,DP05_0070E,DP05_0038E,DP05_0044E,DP05_0035E,DP04_0110E,DP04_0111E,DP04_0112E,DP04_0113E,DP04_0114E,DP04_0115E,DP03_0074E&for=county:#{county}&in=state:#{state}&key=#{@@api_key}"
+    def self.import_county_data_from_api
+        url = "https://api.census.gov/data/2018/acs/acs5/profile?get=NAME,DP05_0001E,DP05_0018E,DP03_0062E,DP05_0037E,DP05_0070E,DP05_0038E,DP05_0044E,DP05_0035E,DP04_0110E,DP04_0111E,DP04_0112E,DP04_0113E,DP04_0114E,DP04_0115E,DP03_0074E&for=county:*&in=state:*&key=#{@@api_key}"
 
         response = HTTParty.get(url).parsed_response
-        response = response.delete_at(0)
+        response.shift
         response
     end
         
     def self.get_demographic_data_by_county
-        counties = {}
+        counties = []
 
         import_county_data_from_api.each do |county_data|
-            county_hash = {}
             county_hash = {
                 :name => county_data[0], 
                 :tot_pop => county_data[1],
@@ -49,10 +48,15 @@ class DataImporter
                 :num_units_using_25_30_percent_monthly_costs_for_mortgage => county_data[12], 
                 :num_units_using_30_35_percent_monthly_costs_for_mortgage => county_data[13], 
                 :num_units_using_35_plus_percent_monthly_costs_for_mortgage => county_data[14], 
-                :tot_num_households_with_food_stamps_or_snap_benefits => county_data[15]
+                :tot_num_households_with_food_stamps_or_snap_benefits => county_data[15],
+                :state_num => county_data[16],
+                :county_num => county_data[17]
             }
-            counties << county
+            counties << county_hash
         end
+        binding.pry
         counties
     end
 end
+
+maryland = DataImporter.get_demographic_data_by_county
--- lib/county_data.rb
+++ lib/county_data.rb
@@ -1,5 +1,5 @@
 class CountyData
-    attr_accessor :name, :tot_pop, :median_age, :med_household_income, :white_pop, :hisp_lat_pop, :black_pop, :asian_pop, :tot_pop_two_plus_races, :num_household_units, :num_units_using_less_than_20_percent_monthly_costs_for_mortgage, :num_units_using_20_25_percent_monthly_costs_for_mortgage, :num_units_using_25_30_percent_monthly_costs_for_mortgage, :num_units_using_30_35_percent_monthly_costs_for_mortgage, :num_units_using_35_plus_percent_monthly_costs_for_mortgage, :tot_num_households_with_food_stamps_or_snap_benefits
+    attr_accessor :name, :tot_pop, :median_age, :med_household_income, :white_pop, :hisp_lat_pop, :black_pop, :asian_pop, :tot_pop_two_plus_races, :num_household_units, :num_units_using_less_than_20_percent_monthly_costs_for_mortgage, :num_units_using_20_25_percent_monthly_costs_for_mortgage, :num_units_using_25_30_percent_monthly_costs_for_mortgage, :num_units_using_30_35_percent_monthly_costs_for_mortgage, :num_units_using_35_plus_percent_monthly_costs_for_mortgage, :tot_num_households_with_food_stamps_or_snap_benefits, :state_num, :county_num
 
     @@all = [ ]
 
@@ -8,5 +8,12 @@ class CountyData
         @@all << self
     end
 
+    def self.create_from_collection(county_array)
+        students_array.each {|county_hash| CountyData.new(county_hash)}
+    end
+
+    def self.all
+        @@all 
+    end
   
 end
