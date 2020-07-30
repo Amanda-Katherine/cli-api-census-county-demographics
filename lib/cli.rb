077b2a3247b28aaab350b17d283411675e82fb6b
Amanda_Katherine <walker.akw@gmail.com>
2020-07-30 00:27:14 -0400

    updates call method in the cli CountyDataController class.

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,28 +8,25 @@ class CountyDataController
     def call
         puts "Welcome to the US Census County Level Demographic Data Library. The data here is from the 2018 American Community Survey 5 Year Data. There are over 3,000 counties in the United States. You might be thinking, wow that's a lot of counties, but you should also be thinking wow, that's a lot of data! Please be patient while the data is pulled from the API. This can take upwards of 30 seconds. I'm sure you're excited to explore some data, so let's jump in!!"
         puts ""
-        puts "To find your county by its name, enter the number '1'"
-        puts "To find all counties within a state, enter the number '2'"
         puts "To exit this program, enter 'exit'"
 
         input = ""
         until input == "exit"
-            puts "What data would you like to explore?"
-            input = gets.strip
+            puts "What state is your county in?"
+            input = gets.strip.split.collect{|w| w.capitalize}.join(" ")
 
-            case input
-
+            if CountyData.find_county_by_state(input) != nil
+                list_counties(input)
+            else
+                puts "It looks like you did not enter a state.     
             end
         end
         
     end
 
 
-    def list_counties_by_state
-        user_state = gets.chomp()
-        user_state = user_state.split.collect{|w| w.capitalize}.join(" ")
-        # if state = CountyData
-        # CountyData.
-    end
+    # def list_counties(input)
+        
+    # end
     
 end
--- lib/county_data.rb
+++ lib/county_data.rb
@@ -17,63 +17,60 @@ class CountyData
     def self.all
         @@all 
     end
-    #will this type of method work? or would I need to do something more similar to this?
-    #state.name.split(",").collect{|w| w.capitalize}.join(" ")
 
     def self.find_county_by_state(state_name)
-        state_name = state_name.split.collect{|w| w.capitalize}.join(" ")
-        self.all.select {|state| state.name.split(", ")[1] == state_name}
+        if state_name != nil
+            state_name = state_name.split.collect{|w| w.capitalize}.join(" ")
+            self.all.select {|state| state.name.split(", ")[1] == state_name}
+        end
     end
    
-    def self.find_by_county_name_all_data(county_name)
+    def self.find_by_county_name_all_variables(county_name)
         county_name = county_name.split.collect{|w| w.capitalize}.join(" ")
         self.all.select {|county| county.name.split(", ")[0].split(" ").collect{|w| w.capitalize}.join(" ") == county_name}    
-    end
+    end 
+end 
+#Worcester County, Maryland
+# counties_array = [{:name=>"Baltimore city, Rhode Island",
+#     :tot_pop=>"51564",
+#     :median_age=>"50.1",
+#     :med_household_income=>"61145",
+#     :white_pop=>"42778",   
+#     :hisp_lat_pop=>"51564",
+#     :black_pop=>"6754",    
+#     :asian_pop=>"685",     
+#     :tot_pop_two_plus_races=>"1042",
+#     :num_household_units=>"9519",
+#     :num_units_using_less_than_20_percent_monthly_costs_for_mortgage=>"3421",
+#     :num_units_using_20_25_percent_monthly_costs_for_mortgage=>"1264",
+#     :num_units_using_25_30_percent_monthly_costs_for_mortgage=>"1090",
+#     :num_units_using_30_35_percent_monthly_costs_for_mortgage=>"876",
+#     :num_units_using_35_plus_percent_monthly_costs_for_mortgage=>"2868",
+#     :tot_num_households_with_food_stamps_or_snap_benefits=>"2218",
+#     :state_num=>"24",
+#     :county_num=>"047"},
+#    {:name=>"baltimore COUNTY, Maryland",
+#     :tot_pop=>"614700",
+#     :median_age=>"35.1",
+#     :med_household_income=>"48840",
+#     :white_pop=>"187152",
+#     :hisp_lat_pop=>"614700",
+#     :black_pop=>"383918",
+#     :asian_pop=>"15898",
+#     :tot_pop_two_plus_races=>"15325",
+#     :num_household_units=>"76658",
+#     :num_units_using_less_than_20_percent_monthly_costs_for_mortgage=>"32082",
+#     :num_units_using_20_25_percent_monthly_costs_for_mortgage=>"11071",
+#     :num_units_using_25_30_percent_monthly_costs_for_mortgage=>"7764",
+#     :num_units_using_30_35_percent_monthly_costs_for_mortgage=>"5754",
+#     :num_units_using_35_plus_percent_monthly_costs_for_mortgage=>"19987",
+#     :tot_num_households_with_food_stamps_or_snap_benefits=>"58388",
+#     :state_num=>"24",
+#     :county_num=>"510"}]
+# baltimore = CountyData.create_from_collection(counties_array)
 
-    
-     
-    
-end #Worcester County, Maryland
-counties_array = [{:name=>"Baltimore city, Rhode Island",
-    :tot_pop=>"51564",
-    :median_age=>"50.1",
-    :med_household_income=>"61145",
-    :white_pop=>"42778",   
-    :hisp_lat_pop=>"51564",
-    :black_pop=>"6754",    
-    :asian_pop=>"685",     
-    :tot_pop_two_plus_races=>"1042",
-    :num_household_units=>"9519",
-    :num_units_using_less_than_20_percent_monthly_costs_for_mortgage=>"3421",
-    :num_units_using_20_25_percent_monthly_costs_for_mortgage=>"1264",
-    :num_units_using_25_30_percent_monthly_costs_for_mortgage=>"1090",
-    :num_units_using_30_35_percent_monthly_costs_for_mortgage=>"876",
-    :num_units_using_35_plus_percent_monthly_costs_for_mortgage=>"2868",
-    :tot_num_households_with_food_stamps_or_snap_benefits=>"2218",
-    :state_num=>"24",
-    :county_num=>"047"},
-   {:name=>"baltimore COUNTY, Maryland",
-    :tot_pop=>"614700",
-    :median_age=>"35.1",
-    :med_household_income=>"48840",
-    :white_pop=>"187152",
-    :hisp_lat_pop=>"614700",
-    :black_pop=>"383918",
-    :asian_pop=>"15898",
-    :tot_pop_two_plus_races=>"15325",
-    :num_household_units=>"76658",
-    :num_units_using_less_than_20_percent_monthly_costs_for_mortgage=>"32082",
-    :num_units_using_20_25_percent_monthly_costs_for_mortgage=>"11071",
-    :num_units_using_25_30_percent_monthly_costs_for_mortgage=>"7764",
-    :num_units_using_30_35_percent_monthly_costs_for_mortgage=>"5754",
-    :num_units_using_35_plus_percent_monthly_costs_for_mortgage=>"19987",
-    :tot_num_households_with_food_stamps_or_snap_benefits=>"58388",
-    :state_num=>"24",
-    :county_num=>"510"}]
-baltimore = CountyData.create_from_collection(counties_array)
+# baltimore_state = CountyData.find_county_by_state("Maryland")
 
-baltimore_state = CountyData.find_county_by_state("Maryland")
-
-baltimore_county = CountyData.find_by_county_name("Baltimore")
+# baltimore_county = CountyData.find_by_county_name("Baltimore")
     
-# binding.pry
+# # binding.pry
